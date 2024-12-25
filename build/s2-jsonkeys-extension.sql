670dbd1f341365ba27ae37d46ea537d6b9294bc1
Michael Englert <m.e@tuta.com>
2024-12-25 10:40:38 +0100

    added ignore files adjusted sql load

--- build/s2-jsonkeys-extension.sql
+++ build/s2-jsonkeys-extension.sql
@@ -1,11 +1,11 @@
-CREATE FUNCTION jsonkeys_table(json longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, expr array(text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL)) 
-RETURNS TABLE(table_col text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL)
+CREATE FUNCTION jsonkeys_table(json LONGTEXT, expr array(TEXT)) 
+RETURNS TABLE(table_col TEXT)
 AS WASM FROM LOCAL INFILE "extension.wasm"
 WITH WIT FROM LOCAL INFILE "extension.wit"
 USING EXPORT 'jsonkeys-table';
 
-CREATE FUNCTION jsonkeys_scalar(json longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, expr array(text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL)) 
-RETURNS text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL 
+CREATE FUNCTION jsonkeys_scalar(json LONGTEXT, expr array(TEXT)) 
+RETURNS TEXT
 AS WASM FROM LOCAL INFILE "extension.wasm"
 WITH WIT FROM LOCAL INFILE "extension.wit"
 USING EXPORT 'jsonkeys-scalar';
